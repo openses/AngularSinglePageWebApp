22d588310982a23816656c42c28898ffb27ce184
buerojacob <claude.jacob@buerojacob.ch>
2018-10-09 10:16:56 +0200

    ok

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -5,6 +5,8 @@ export class Globals {
   selectedTab: any = 'Tab01';
   selectedLanguage: any = 'de';
   selectedCommunity: any = 'app';
-  myhtmlpath: any = 'http://buerojacob.ch/openses-content/';
+  // myhtmlpath: any = 'http://buerojacob.ch/openses-content/';
+  myhtmlpath: any = 'https://openses.org/openses-content/';
   myhtmlfile: any = 'openses_01_00.html';
+  myID: any = 'T1';
 }
